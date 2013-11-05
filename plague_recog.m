c9341fd3e6c5542dc70a83361b1131b2fc0d48fe
WiZardLancet <dddwzl3703@163.com>
2013-11-06 00:26:03 +0800

    test pics

--- plague_recog.m
+++ plague_recog.m
@@ -6,16 +6,29 @@ I=imread(filename);
 Id=im2double(I);
 %need resize image here
 back=grep_background(Id);
+Is=zeros(size(I));
 for i=1:3
     Is(:,:,i)=Id(:,:,i)-back(:,:,i);
 end
 
-[Amat inter]=select_and_regress(Is);
+[Amat,inter]=select_and_regress(Is);
 Ilog=rgb2logit(Is,Amat,inter);
 Ibw=im2bw(Ilog,0);
-M=strel('disk',5);
+M=strel('disk',3);
 Iclose=imclose(Ibw,M);
 
+label=bwlabel(~Iclose);
+A=regionprops(label);
+square=cat(1,A.BoundingBox);
+square=square(:,3:4);
+square=square(:,1).*square(:,2);
+area=cat(1,A.Area);
+area_square_ratio=area./square;
+a=abs(area_square_ratio-0.7854);
+
+rgblabel=label2rgb(label,[a a a]);
+imshow(rgblabel);
 result=Iclose;
+%to count
 end
 
